4f30a9e8333c592076b91a98e484371fc55727eb
fed-gren <deveb22@gmail.com>
2018-10-21 23:19:34 +0900

    frontend 폴더 구조 수정

--- vin/src/main/java/kr/ac/hongik/vin/config/WebMvcContextConfiguration.java
+++ vin/src/main/java/kr/ac/hongik/vin/config/WebMvcContextConfiguration.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package kr.ac.hongik.vin.config;
 
 import org.springframework.context.annotation.Bean;
@@ -40,55 +39,8 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         //resolver.setPrefix("/WEB-INF/views/");
         //resolver.setSuffix(".jsp");
-        resolver.setPrefix("frontend/");
+        resolver.setPrefix("/../resources/static/");
         resolver.setSuffix(".html");
         return resolver;
     } 
 }
-=======
-package kr.ac.hongik.vin.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages= {"kr.ac.hongik.vin.controller"})
-public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
-	@Override
-	// Resource들을 handling하는 핸들러를 선언 및 세팅
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/img/**").addResourceLocations("/resources/img/").setCachePeriod(31556926);
-        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/").setCachePeriod(31556926);
-        registry.addResourceHandler("/txt/**").addResourceLocations("/resources/txt/").setCachePeriod(31556926);
-    }
-	
-    // default servlet handler를 사용하게 합니다.
-    @Override	// 아무런 핸들러가 정해지지 않았을 때, 사용하는 default servlet handler의 설정
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-   
-    @Override	// default Controller를 설정.
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-    }
-    
-    @Bean	// 기본 경로는 src/main/webapp/WEB-INF/views 디렉토리이다.
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    } 
-}
->>>>>>> c3bbc1c3034190fab947b57e3cd25e038328ec07
